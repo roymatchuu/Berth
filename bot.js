f925ec818aa89abb8f796a529dece9d60970cf57
unknown <rmramosvii@gmail.com>
2020-08-03 12:39:45 -0700

    Added a Command Handler system

--- bot.js
+++ bot.js
@@ -1,24 +1,48 @@
-const { Client } = require("discord.js");
-const { config } = require("dotenv");
-const client = new Client();
-
+// bot delimeter 
 const delim = "`";
+
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+// uses dotenv to extract stored credentials 
+const { config } = require("dotenv");
 
 config({
     path: __dirname + "/.env"
 });
 
-client.on("ready", () => {
-    console.log(`${client.user.username} is online!`);
-    client.user.setActivity('Birthdays', { type: 'WATCHING' });
+// Command Handler
+const fs = require('fs');
+bot.commands = new Discord.Collection();
+
+// signify where the command files are
+const cmdFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of cmdFiles){
+    const cmd = require(`./commands/${file}`);
+
+    bot.commands.set(cmd.name, cmd);
+}
+
+
+// Bot initialized 
+bot.on("ready", () => {
+    console.log(`${bot.user.username} is online!`);
+    bot.user.setActivity('Birthdays', { type: 'WATCHING' });
 });
 
-client.on('message', msg => {
-    if (msg.content === `${delim}ping`) {
-    //   msg.reply('pong');
-      msg.channel.send('Berth is awake!');
+bot.on('message', msg => {
+    if(!msg.content.startsWith(delim)) return;
+
+    let args = msg.content.substring(delim.length).split(" "); 
+    
+    switch(args[0]){
+        case "ping":
+            bot.commands.get('ping').execute(msg, args);
+            break; 
+        
+
     }
 });
 
-
-client.login(process.env.TOKEN);
+// activate the bot using a token 
+bot.login(process.env.TOKEN);
---
+++ commands/ping.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'ping', 
+    description: 'informs whether the bot is working or not',
+    execute(msg, args){
+        msg.channel.send('Berth is awake!');
+    }
+}
